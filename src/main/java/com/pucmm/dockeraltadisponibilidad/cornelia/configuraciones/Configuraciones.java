f6da3934e921aec17def40ae64ce5785bb031ccb
Juan Thomás Ángel Montilla <juan.thomas.angel@gmail.com>
2019-07-27 02:04:43 -0400

    agregando configuración de seguridad de spring security

---
+++ src/main/java/com/pucmm/dockeraltadisponibilidad/cornelia/configuraciones/Configuraciones.java
@@ -0,0 +1,6 @@
+package com.pucmm.dockeraltadisponibilidad.cornelia.configuraciones;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class Configuraciones extends WebSecurityConfigurerAdapter {
+}
