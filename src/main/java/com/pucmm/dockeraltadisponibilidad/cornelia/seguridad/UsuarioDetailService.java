8367b2107d0c178508d684b4cee2e72190f5b479
Juan Thomás Ángel Montilla <juan.thomas.angel@gmail.com>
2019-07-27 02:03:35 -0400

    Agregando manejo de seguridad de spring security

--- src/main/java/com/pucmm/dockeraltadisponibilidad/cornelia/seguridad/UsuarioDetailService.java
+++ src/main/java/com/pucmm/dockeraltadisponibilidad/cornelia/seguridad/UsuarioDetailService.java
@@ -1,4 +1,25 @@
 package com.pucmm.dockeraltadisponibilidad.cornelia.seguridad;
 
-public class UsuarioDetailService {
+import com.pucmm.dockeraltadisponibilidad.cornelia.entidades.Usuario;
+import com.pucmm.dockeraltadisponibilidad.cornelia.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipal;
+
+@Service
+public class UsuarioDetailService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioDetailService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsuario(username);
+        return UsuarioPrincipal.crear(usuario);
+    }
 }
--- src/main/java/com/pucmm/dockeraltadisponibilidad/cornelia/seguridad/UsuarioPrincipal.java
+++ src/main/java/com/pucmm/dockeraltadisponibilidad/cornelia/seguridad/UsuarioPrincipal.java
@@ -1,4 +1,73 @@
 package com.pucmm.dockeraltadisponibilidad.cornelia.seguridad;
 
-public class UsuarioPrincipal {
+import com.pucmm.dockeraltadisponibilidad.cornelia.entidades.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Objects;
+
+public class UsuarioPrincipal implements UserDetails {
+
+    private String usuario;
+    private String contraisenia;
+
+    public UsuarioPrincipal(String username, String password) {
+        this.usuario = username;
+        this.usuario = password;
+    }
+
+    public static UsuarioPrincipal crear(Usuario usuario) {
+        return new UsuarioPrincipal(usuario.getUsuario(), usuario.getContraisenia());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return contraisenia;
+    }
+
+    public void setPassword(String contraisenia) {
+        this.contraisenia = contraisenia;
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario;
+    }
+
+    public void setUsername(String username) {
+        this.usuario = username;
+    }
+
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(usuario);
+    }
+
 }
