020fb16f9c23df41458895e2c99d9f05b34d87ba
Juan Thomás Ángel Montilla <juan.thomas.angel@gmail.com>
2019-07-29 18:41:14 -0400

    modificando docker compose

--- src/main/java/com/pucmm/dockeraltadisponibilidad/cornelia/entidadPublica.java
+++ src/main/java/com/pucmm/dockeraltadisponibilidad/cornelia/entidadPublica.java
@@ -6,3 +6,39 @@ public class entidadPublica {
     public Integer resultado3;
     public Integer resultado4;
 }
+
+  /*cornelia2-service:
+          restart: always
+          image: repdom/docker-cornelia-app
+          environment:
+          - SERVER_PORT=8081
+          networks:
+          - cornelia
+          depends_on:
+          - mariadb
+          ports:
+          - "8081"
+          cornelia3-service:
+          restart: always
+          image: repdom/docker-cornelia-app
+          environment:
+          - SERVER_PORT=8082
+          networks:
+          - cornelia
+          depends_on:
+          - mariadb
+          ports:
+          - "8082"
+          ha-proxy:
+          restart: always
+          networks:
+          - cornelia
+          depends_on:
+          - cornelia-service
+          - cornelia2-service
+          - cornelia3-service
+          image: haproxy:2.0.2
+          volumes:
+          - ./etc:/usr/local/etc/haproxy
+          ports:
+          - "81:80"*/
